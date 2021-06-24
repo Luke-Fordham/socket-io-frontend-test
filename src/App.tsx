e7878a6e5696d07f464b7a83c040a1f1b3879e6e
luke <luke@blueskylabs.com.au>
2021-06-24 11:00:13 +0800

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
+import React, {useEffect, useState} from "react";
 import "./styles.css";
+import socket from './socket'
 
 export default function App() {
-  return (
+    const [username, setUsername] = useState<string|null>(null);
+
+    useEffect(() => {
+        socket.onAny((event, ...args) => {
+            console.log(event, args);
+        });
+        socket.on("connect_error", (err) => {
+            if (err.message === "invalid username") {
+                setUsername(null);
+            }
+        });
+    }, [])
+
+    const handleSubmit = () => {
+        if (username){
+            const user = username.split(' ').join('').replace(' ', '');
+            if (user && user.length > 0) {
+                socket.auth = {username: user};
+                socket.connect();
+            }
+        }
+    }
+
+    return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Input username</h1>
+        {/*@ts-ignore*/}
+        <input value={username ? username : ''} placeholder={'Your username'} onChange={(e) => setUsername(e.target.value)}/>
+        <button onClick={handleSubmit}>Send</button>
     </div>
   );
 }
