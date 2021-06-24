1500fb1fd1bd137aa15351c66e7dd8cb20f71c04
luke <luke@blueskylabs.com.au>
2021-06-24 16:16:12 +0800

    can send message - not yet displayed to other user

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -12,6 +12,9 @@ interface IUser {
 const Chat = () => {
 
     const [users, setUsers] = useState<IUser[]>();
+    const [selectedUserId, setSelectedUserId] = useState<string>();
+    const [sendMessage, setMessage] = useState<string>();
+    const [messageList, setMessageList] = useState<{content: string, fromSelf: boolean}[]>([]);
 
     useEffect(() => {
         users && console.log("USERS: \n", users);
@@ -44,15 +47,49 @@ const Chat = () => {
             setUsers(newUserList);
     });
 
+    const handleUserSelect = (e: any) => {
+        setSelectedUserId(e.value);
+    }
+
+    const handleSend = () => {
+        const sanitised = sendMessage?.split(' ').join('');
+        if (selectedUserId && sendMessage && sanitised && sanitised.length > 0) {
+            socket.emit("private message", {
+                sendMessage,
+                to: selectedUserId
+            });
+            const newMessageList = [...messageList];
+            newMessageList.push({content: sendMessage, fromSelf: true})
+            setMessageList(newMessageList);
+        }
+    }
+
     return (
         <>
-        <div>
-            <Select options={users && users.map(user => {
-                if (user.self){
-                    return {label: `${user.username} (you)`, value: user.userID}
-                }
-                return {label: user.username, value: user.userID}
-            })} />
+        <div style={{width: '300px', margin: 'auto'}}>
+            <div style={{width: '300px', margin: 'auto'}}>
+                <Select onChange={handleUserSelect} options={users && users.map(user => {
+                    if (user.self){
+                        return {label: `${user.username} (you)`, value: user.userID}
+                    }
+                    return {label: user.username, value: user.userID}
+                })} />
+            </div>
+            <div>
+                {messageList.map(message => {
+                    let color = 'grey';
+                    if (message.fromSelf) color = '#35b0f0';
+                    return (
+                      <div style={{backgroundColor: color, color: 'white', padding: '10px'}}>{message.content}</div>
+                    );
+                })}
+            </div>
+            <div style={{margin: '100px auto 0', width: 'fit-content'}}>
+                <textarea onChange={(e) => setMessage(e.target.value)} style={{height: '50px', width: '300px'}}/>
+                <div style={{margin: 'auto 0 auto auto'}}>
+                    <button onClick={handleSend} style={{float: 'right'}}>Send</button>
+                </div>
+            </div>
         </div>
         </>
     );
