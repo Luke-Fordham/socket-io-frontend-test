a5a11b6e948476268d47cb7e195a44c4bb0cc0cc
luke <luke@blueskylabs.com.au>
2021-07-05 16:26:28 +0800

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,45 +2,57 @@ import React, {useEffect, useState} from "react";
 import "./styles.css";
 import socket from './socket'
 import Chat from './components/Chat'
+import Select from 'react-select'
+
+export interface IUser {
+    id: number;
+    username: string;
+}
 
 export default function App() {
-    const [username, setUsername] = useState<string | null>(null);
-    const [usernameSelected, setUsernameSelected] = useState<boolean>(false);
+    const [user, setUser] = useState<IUser>(null);
+    const [userSelected, setUserSelected] = useState<boolean>(false);
+    const [users, setUsers] = useState<IUser[]>();
 
     useEffect(() => {
-        console.log(usernameSelected);
+        const pullUsers = async () => {
+            const response = await fetch('http://localhost:8080/get-all-users');
+            const results = await response.json();
+            console.log(results);
+            if (results.success) {
+                setUsers(results.users)
+            }
+        }
+        pullUsers();
+
         socket.onAny((event, ...args) => {
             console.log(event, args);
         });
         socket.on("connect_error", (err) => {
             if (err.message === "invalid username") {
-                setUsernameSelected(false);
+                setUserSelected(false);
             }
         });
     }, [])
 
     const handleSubmit = () => {
-        if (username) {
-            const user = username.split(' ').join('').replace(' ', '');
-            if (user && user.length > 0) {
-                setUsernameSelected(true);
-                socket.auth = {username: user};
+        if (user) {
+                setUserSelected(true);
+                socket.auth = {username: user.username, userId: user.id};
                 socket.connect();
-            }
         }
     }
 
     return (
         <div className="App">
             {/*@ts-ignore*/}
-            {!usernameSelected ?
+            {!userSelected ?
                 <>
-                    <h1>Input username</h1>
-                    <input value={username ? username : ''} placeholder={'Your username'}
-                           onChange={(e) => setUsername(e.target.value)}/>
+                    <h1>Select user</h1>
+                    <Select onChange={({value}) => setUser(users[Number(value)])} options={ users && users.length > 0 && users.map(user => { return {value: String(user.id), label: user.username}})}/>
                     <button onClick={handleSubmit}>Send</button>
                 </> :
-                <Chat />
+                <Chat user={user} />
             }
         </div>
     );
--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -12,13 +12,8 @@ export interface IUser {
     connected?: boolean;
 }
 
-const Chat = () => {
+const Chat = ({user}) => {
     const [users, setUsers] = useState<IUser[]>();
-    const [connected, setConnected] = useState<string>('none');
-
-    const initReactiveProperties = (user: IUser) => {
-        user.hasNewMessages = false;
-    };
 
     useEffect(() => {
         users && console.log("USERS: \n", users);
@@ -28,7 +23,6 @@ const Chat = () => {
         socket.on("users", (users: IUser[]) => {
             users.forEach((user) => {
                 user.self = user.userID === socket.id;
-                initReactiveProperties(user);
             });
             // put the current user first, and then sort by username
             const sortedUsers = users.sort((a, b) => {
@@ -41,34 +35,11 @@ const Chat = () => {
         });
 
         socket.on("user connected", (user) => {
-            initReactiveProperties(user);
             const newUserList = users ? [...users] : [];
             newUserList.push(user);
             setUsers(newUserList);
         });
     }, [users])
-
-    // socket.on("connect", () => {
-    //     const newList = users && [...users];
-    //     newList && newList.length > 0 && newList.forEach((user, i) => {
-    //         if (user.self) {
-    //             user.connected = true;
-    //         }
-    //         newList[i] = user;
-    //         setUsers(newList);
-    //     });
-    // });
-    //
-    // socket.on("disconnect", () => {
-    //     const newList = users && [...users];
-    //     newList && newList.length > 0 && newList.forEach((user, i) => {
-    //         if (user.self) {
-    //             user.connected = false;
-    //         }
-    //         newList[i] = user;
-    //         setUsers(newList);
-    //     });
-    // });
 
     return (
         <>
@@ -84,7 +55,7 @@ const Chat = () => {
                 </div>
                 <div style={{width: '100%', margin: 'auto'}}>
                     <div
-                        style={{margin: 'auto', border: `2px solid ${connected}`, borderRadius: '5px', padding: '20px'}}>
+                        style={{margin: 'auto', borderRadius: '5px', padding: '20px'}}>
                         <Route path={'/'}>
                             <h1>ChatApp</h1>
                         </Route>
