266f6c5bd9e1851d1f0410b96d7a38e500d673fb
luke <luke@blueskylabs.com.au>
2021-07-28 11:02:55 +0800

    create new conversation with selected members - frontend replacing last added conversation - backend working okay

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,7 +1,9 @@
-import React, {useEffect, useState} from 'react';
+import React, {CSSProperties, useEffect, useState} from 'react';
 import socket from '../socket';
 import {Link, Switch, Route, BrowserRouter as Router} from 'react-router-dom'
 import Room from './Room';
+import Select from "react-select";
+import {UserOption} from "../App";
 
 export interface IUser {
     hasNewMessages: boolean;
@@ -13,34 +15,23 @@ export interface IUser {
 }
 
 const Chat = () => {
-    const [users, setUsers] = useState<IUser[]>();
     const [conversations, setConversations] = useState<any>();
+    const [showModal, setShowModal] = useState<boolean>(false);
+    const [userOptions, setUserOptions] = useState<UserOption[]>([]);
+    const [addingMembers, setAddingMembers] = useState<UserOption[]>();
+    const [newConvName, setNewConvName] = useState<string>(null);
 
     useEffect(() => {
-        users && console.log("USERS: \n", users);
-    }, [users])
-
-    useEffect(() => {
-        socket.on("users", (users: IUser[]) => {
-            users.forEach((user) => {
-                user.self = user.userID === socket.id;
-            });
-            // put the current user first, and then sort by username
-            const sortedUsers = users.sort((a, b) => {
-                if (a.self) return -1;
-                if (b.self) return 1;
-                if (a.username < b.username) return -1;
-                return a.username > b.username ? 1 : 0;
-            });
-            setUsers(sortedUsers);
-        });
-
-        socket.on("user connected", (user) => {
-            const newUserList = users ? [...users] : [];
-            newUserList.push(user);
-            setUsers(newUserList);
-        });
-    }, [users])
+        const pullUsers = async () => {
+            const response = await fetch('http://localhost:3001/get-all-users');
+            const results = await response.json();
+            console.log(results);
+            if (results.success) {
+                setUserOptions(results.users.map(user => { return {value: user.id, label: user.username}}))
+            }
+        }
+        pullUsers();
+    }, [])
 
     useEffect(() => {
         socket.on('conversations', (e) => {
@@ -52,42 +43,95 @@ const Chat = () => {
     }, [conversations])
 
     const addConversation = () => {
-        socket.emit('new conversation', {
-            name: 'new conversation',
-            members: [1, 2]
-        })
+        if (addingMembers && addingMembers.length > 0){
+            console.log({
+                name: newConvName,
+                members: addingMembers
+            })
+            socket.emit('new conversation', {
+                name: newConvName,
+                members: addingMembers
+            })
+        }
+    }
+
+    const modalBackground: CSSProperties = {
+        top: '0',
+        right: '0',
+        bottom: '0',
+        left: '0',
+        backgroundColor: '#00000082',
+        position: 'absolute',
+        zIndex: 100
+    }
+
+    const modalContent: CSSProperties = {
+        margin: '10% auto auto auto',
+        top: '0',
+        right: '0',
+        bottom: '0',
+        left: '0',
+        width: '40vw',
+        height: '50vh',
+        backgroundColor: 'white',
+        position: 'absolute',
+        zIndex: 101
     }
 
     return (
         <>
             <Router>
-                <div style={{display: 'grid', gridTemplateColumns: '1fr 4fr'}}>
-                <div style={{display: 'flex', flexDirection: 'column', border: '2px solid rgb(181 221 226)', boxShadow: '0px 0px 5px 0px rgba(0,0,0,0.15)', padding: '5px', borderRadius: '3px'}}>
-                    <h3>Conversations</h3>
-                    {/*{users && users.map(user => <Link*/}
-                    {/*    style={{padding: '10px', textDecoration: 'none', color: '#1d8694', boxShadow: '0px 0px 5px 0px rgba(0,0,0,0.15)', margin: '5px', borderRadius: '3px', border: '2px solid rgb(181 221 226)'}}*/}
-                    {/*    to={`/conversation/${user.userID}`}>*/}
-                    {/*    <span>{user.self ? `${user.username} (you)` : user.username}</span>*/}
-                    {/*</Link>)}*/}
-                    {conversations && conversations.map(convo => <Link
-                        style={{padding: '10px', textDecoration: 'none', color: '#1d8694', boxShadow: '0px 0px 5px 0px rgba(0,0,0,0.15)', margin: '5px', borderRadius: '3px', border: '2px solid rgb(181 221 226)'}}
-                        to={`/conversation/${convo.id}`}>
-                        <span>{convo.name}</span>
-                    </Link>)}
-                    <button onClick={addConversation}>New Conversation</button>
-                </div>
-                <div style={{width: '100%', margin: 'auto'}}>
-                    <div
-                        style={{margin: 'auto', borderRadius: '5px', padding: '20px'}}>
-                        <Route path={'/'}>
-                        </Route>
-                        <Switch>
-                            <Route exact path='/conversation/:id/'>
-                                <Room />
-                            </Route>
-                        </Switch>
+                {showModal &&
+                    <>
+                    <div style={modalContent}>
+                        <div style={{position: 'relative', width: '100%', height: '100%'}}>
+                            <Select isMulti options={userOptions} onChange={(e: UserOption[]) => setAddingMembers(e)}/>
+                            <button style={{display: 'block', margin: 'auto', position: 'absolute', bottom: '20px', right: '20px'}} onClick={addConversation}>
+                                Add conversation
+                            </button>
+                        </div>
                     </div>
-                </div>
+                    <div style={modalBackground} onClick={() => setShowModal(false)}>
+                    </div>
+                    </>
+                }
+                <div style={{display: 'grid', gridTemplateColumns: '1fr 4fr'}}>
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        border: '2px solid rgb(181 221 226)',
+                        boxShadow: '0px 0px 5px 0px rgba(0,0,0,0.15)',
+                        padding: '5px',
+                        borderRadius: '3px'
+                    }}>
+                        <h3>Conversations</h3>
+                        {conversations && conversations.map(convo => <Link
+                            style={{
+                                padding: '10px',
+                                textDecoration: 'none',
+                                color: '#1d8694',
+                                boxShadow: '0px 0px 5px 0px rgba(0,0,0,0.15)',
+                                margin: '5px',
+                                borderRadius: '3px',
+                                border: '2px solid rgb(181 221 226)'
+                            }}
+                            to={`/conversation/${convo.id}`}>
+                            <span>{convo.name}</span>
+                        </Link>)}
+                        <button onClick={() => setShowModal(true)}>New Conversation</button>
+                    </div>
+                    <div style={{width: '100%', margin: 'auto'}}>
+                        <div
+                            style={{margin: 'auto', borderRadius: '5px', padding: '20px'}}>
+                            <Route path={'/'}>
+                            </Route>
+                            <Switch>
+                                <Route exact path='/conversation/:id/'>
+                                    <Room/>
+                                </Route>
+                            </Switch>
+                        </div>
+                    </div>
                 </div>
             </Router>
         </>
